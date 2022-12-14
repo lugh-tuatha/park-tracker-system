f1686981dde5242e0cf2ae6eb59603f2b6052762
Ace Gabriel P. Pasiliao <114063817+lugh-tuatha@users.noreply.github.com>
2022-12-14 16:26:37 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// increment parking entered
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+  count = count + 1;
+  countEl.innerText = count;
+}
+
+// form to google sheets
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwPPibASEByK4hGrShfN4GNUORos9qCNaUrxxpCcztVUoQFr4wfVFR56_uDak5pZyUJAQ/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
+
